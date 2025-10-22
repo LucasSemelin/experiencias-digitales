9d28905c8833dff517b72f65167cbaa8c3181bf6
Lucas Semelin <lucas@lucassemelin.com>
2025-10-22 17:56:06 -0300

    Fix trailing slash in app button

--- app/components/AppButton.vue
+++ app/components/AppButton.vue
@@ -1,7 +1,7 @@
 <template>
     <NuxtLink
         v-if="destination"
-        :to="destination + '/'"
+        :to="destination"
         class="bg-gradient-to-br from-gradient-from to-gradient-to from-10% to-200% hover:from-0% hover:to-80% hover:to-gradient-via transition-all duration-300 text-grey-950 font-medium px-6 py-3 rounded-full text-center block text-sm w-full lg:w-fit relative hover:cursor-pointer"
     >
         <slot />
