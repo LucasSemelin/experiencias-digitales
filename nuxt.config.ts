5ed1a8b9fcd5789da8e69374439dc325cc358b8f
Lucas Semelin <lucas@lucassemelin.com>
2025-10-27 10:40:15 -0300

    Fix tag manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -139,7 +139,7 @@ export default defineNuxtConfig({
     },
     css: ["~/assets/css/app.css"],
     gtag: {
-        id: "GTM-WPK759W3",
+        id: "GTM-PXHVRBL6",
         enabled: process.env.NODE_ENV === "production",
     },
     site: {
